7dc7d2ade1829a8bd6ec21ba219f2ee098ed10d1
seyi-react <oluwaseyiadeoti825@gmail.com>
2024-10-30 12:03:10 +0100

    UI-setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { Text } from '@chakra-ui/react'
+
+
+const App = () => {
+  return (
+    <>
+      <h3>Hello</h3>
+      {/* <Text>Yello</Text> */}
+    </>
+  );
+};
+
+export default App;
